b56a81e995d3334b9b27bc285a7d7953e9684ce6
Julia Nguyen <julia@8thsq.co>
2017-01-18 01:27:22 -0500

    fix navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -21,44 +21,46 @@ class Nav extends React.Component {
       icon: {
         fontSize: ".9rem",
         color: "#6C6C6D"
+      },
+      toggle: {
+        fontSize: ".9rem",
+        marginTop: "5px"
       }
     }
     const toggled = this.state.toggled;
     const handleToggle = this.handleToggle;
     const showInput = this.state.showInput;
     const playlistName = this.props.playlistName;
-    return(
+    return (
       <div>
       <nav className="navbar fixed-top navbar-toggleable-md navbar-light bg-faded">
-      <a className="navbar-brand"><i className="fa fa-headphones"></i>  Top Ten</a>
-        <form className="hidden-md-down form-inline">
-            <button onClick={handleToggle} data-toggle="collapse" data-target="#userPlaylist" className="btn btn-sm btn-outline-primary" type="button">
-              {toggled && <span><i className="fa fa-toggle-on"></i> Hide Playlist</span>}{!toggled && <span><i className="fa fa-toggle-off"></i> Show Playlist</span>}
-            </button>
-        </form>
-        <span onClick={handleToggle} className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-          <button className="btn btn-sm btn-outline-primary" type="button">
-              {toggled && <span><i className="fa fa-toggle-on"></i> Hide Playlist</span>}{!toggled && <span><i className="fa fa-toggle-off"></i> Show Playlist</span>} ({this.props.total})
-            </button>
-        </span>
+        <button style={style.toggle} className="navbar-toggler navbar-toggler-right" onClick={handleToggle} type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            {toggled && <span><i className="fa fa-toggle-on"></i> Hide Playlist</span>}{!toggled && <span><i className="fa fa-toggle-off"></i> Show Playlist</span>} ({this.props.total})
+        </button>
+        <a className="navbar-brand" href="#"><i className="fa fa-headphones"></i>  Top Ten</a>
         <div className="collapse navbar-collapse" id="navbarText">
           <ul className="navbar-nav mr-auto">
-          <div className="hidden-lg-up">{this.props.content}</div>
+          <div className="hidden-md-down">
+            <button style={style.toggle} onClick={handleToggle} data-toggle="collapse" data-target="#userPlaylist" className="btn btn-sm align-middle btn-outline-primary" type="button">
+             {toggled && <span><i className="fa fa-toggle-on"></i> Hide Playlist</span>}{!toggled && <span><i className="fa fa-toggle-off"></i> Show Playlist</span>}
+            </button>
+         </div>
+           <div className="hidden-lg-up">{this.props.content}</div>
           </ul>
           <span className="navbar-text">
             Total Songs: {this.props.total}
-          </span>
+           </span>
         </div>
       </nav>
-          <div className="collapse" id="userPlaylist">
-          <div className="p-4">
-            {!showInput && <h4 style={style} onClick={this.toggleInput}>{playlistName ||  "Playlist" } <i style={style.icon}className="fa fa-pencil-square-o" aria-hidden="true"></i></h4>}
-            {showInput && <input className="form-control" value={playlistName} onChange={this.props.onChange} onBlur={this.toggleInput}/>}
-            {this.props.content}
-          </div>
-        </div>
+      <div className="collapse" id="userPlaylist">
+           <div className="p-4">
+             {!showInput && <h4 style={style} onClick={this.toggleInput}>{playlistName ||  "Playlist" } <i style={style.icon}className="fa fa-pencil-square-o" aria-hidden="true"></i></h4>}
+             {showInput && <input className="form-control" value={playlistName} onChange={this.props.onChange} onBlur={this.toggleInput}/>}
+             {this.props.content}
+           </div>
+         </div>
       </div>
-    )
+      )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // eslint-disable-next-line
-import jquery from 'jquery';
+// import jquery from 'jquery';
 // eslint-disable-next-line
-import bootstrap from 'bootstrap';
+import tether from 'tether';
+
 import App from './App';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import './index.css';
